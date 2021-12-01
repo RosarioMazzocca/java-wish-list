2436b4b31fc7499ea30a19f11d1a56548fffd229
RosarioMazzocca <mazzocca.rosario@libero.it>
2021-12-01 16:25:45 +0100

    Aggiunto programma

---
+++ src/org/generation/italy/christmas/Main.java
@@ -0,0 +1,42 @@
+package org.generation.italy.christmas;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		ArrayList<String> wishlist = new ArrayList<String>();
+
+		boolean continua = true;
+		String si = "s";
+		String no = "n";
+
+		do {
+			System.out.println("Aggiungi elemento alla lista dei desideri: ");
+			wishlist.add(scanner.nextLine());
+			System.out.println("La tua lista contiene " + wishlist.size() + " desideri/o");
+			System.out.println("Continuare? (s/n) ");
+			String siNo = scanner.nextLine();
+			if (siNo.equals(no)) {
+				continua = false;
+			} else if (siNo.equals(si)) {
+				continua = true;
+			} else {
+				System.out.println("Errore: valore non valido. Inserisci 's' oppure 'n' ");
+			}
+		} while (continua);
+		System.out.println("Lista conclusa");
+
+		System.out.println("Ecco la tua lista: ");
+
+		for (int i = 0; i < wishlist.size(); i++) {
+			System.out.println(wishlist.get(i));
+		}
+
+		scanner.close();
+	}
+
+}
